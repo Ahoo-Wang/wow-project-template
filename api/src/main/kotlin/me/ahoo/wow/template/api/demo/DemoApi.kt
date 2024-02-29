682e04aa9346425de5ef49c3adf8d6af370dc5b1
Ahoo Wang <ahoowang@qq.com>
2024-03-01 00:41:51 +0800

    Update scaffolding

--- api/src/main/kotlin/me/ahoo/wow/template/api/demo/DemoApi.kt
+++ api/src/main/kotlin/me/ahoo/wow/template/api/demo/DemoApi.kt
@@ -1,14 +1,7 @@
 package me.ahoo.wow.template.api.demo
 
-import me.ahoo.wow.template.api.demo.query.DemoDto
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.service.annotation.GetExchange
+import me.ahoo.wow.template.DemoService
 import org.springframework.web.service.annotation.HttpExchange
-import reactor.core.publisher.Mono
 
-@HttpExchange("demo")
-interface DemoApi {
-
-    @GetExchange("{id}")
-    fun getById(@PathVariable id: String): Mono<DemoDto>
-}
+@HttpExchange(DemoService.DEMO_AGGREGATE_NAME)
+interface DemoApi
