c7f69f5cfbf2d0e9433e4bdf7414363e38ce0624
Ahoo Wang <ahoowang@qq.com>
2024-03-01 00:42:55 +0800

    Update scaffolding (#74)

--- api/src/main/kotlin/me/ahoo/wow/template/api/demo/query/DemoDto.kt
+++
@@ -1,3 +0,0 @@
-package me.ahoo.wow.template.api.demo.query
-
-data class DemoDto(val id: String)
--- server/src/main/kotlin/me/ahoo/wow/template/server/demo/DemoController.kt
+++ server/src/main/kotlin/me/ahoo/wow/template/server/demo/DemoController.kt
@@ -1,13 +1,7 @@
 package me.ahoo.wow.template.server.demo
 
 import me.ahoo.wow.template.api.demo.DemoApi
-import me.ahoo.wow.template.api.demo.query.DemoDto
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 
 @RestController
-class DemoController : DemoApi {
-    override fun getById(id: String): Mono<DemoDto> {
-        return Mono.just(DemoDto(id))
-    }
-}
+class DemoController : DemoApi
