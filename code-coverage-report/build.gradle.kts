a3a1b100d530304d786a3be75000a5fdec23d679
Ahoo Wang <ahoowang@qq.com>
2025-02-26 10:17:08 +0800

    build(code-coverage-report): update test suite name for code coverage report
    - Change test suite name from 'UNIT_TEST' to 'test' for more accurate reporting

--- code-coverage-report/build.gradle.kts
+++ code-coverage-report/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 reporting {
     reports {
         val codeCoverageReport by creating(JacocoCoverageReport::class) {
-            testType.set(TestSuiteType.UNIT_TEST)
+            testSuiteName = "test"
         }
     }
 }
