8e68edb9f8ed547a8753968f3211227039a4d3ac
Ahoo Wang <ahoowang@qq.com>
2025-02-26 17:32:33 +0800

    refactor(template): update bounded context usage and spring boot application run (#405)
    - Update @BoundedContext usage in DemoService with named arguments
    - Replace runApplication with SpringApplication.run in DemoApplication

--- server/src/main/kotlin/me/ahoo/wow/template/server/DemoApplication.kt
+++ server/src/main/kotlin/me/ahoo/wow/template/server/DemoApplication.kt
@@ -3,8 +3,8 @@ package me.ahoo.wow.template.server
 import me.ahoo.wow.api.annotation.BoundedContext
 import me.ahoo.wow.template.DemoService
 import me.ahoo.wow.template.domain.DemoBoundedContext
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @BoundedContext(name = DemoService.SERVICE_NAME)
 @SpringBootApplication(
@@ -13,5 +13,5 @@ import org.springframework.boot.runApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-    runApplication<DemoApplication>(*args)
+    SpringApplication.run(arrayOf(DemoApplication::class.java), args)
 }
