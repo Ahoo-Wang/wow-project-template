0c642f53c1aeb4214165d0da12f42446d5ca1c26
Ahoo Wang <ahoowang@qq.com>
2025-03-23 08:39:53 +0800

    build(deps): add kotlin-logging dependency
    - Add kotlin-logging to dependencies/build.gradle.kts
    - Include kotlin-logging-jvm in domain/build.gradle.kts
    - Update gradle/libs.versions.toml with kotlin-logging version 7.0.5

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     ksp(platform(project(":dependencies")))
     api(project(":api"))
     api("me.ahoo.wow:wow-spring")
+    api("io.github.oshai:kotlin-logging-jvm")
     ksp("me.ahoo.wow:wow-compiler")
     testImplementation("me.ahoo.wow:wow-test")
     testImplementation("io.projectreactor:reactor-test")
