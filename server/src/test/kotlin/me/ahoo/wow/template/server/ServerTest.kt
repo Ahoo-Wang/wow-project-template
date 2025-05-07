f61d17072b3052622aaafb5193b99c355627ae44
Ahoo Wang <ahoowang@qq.com>
2025-05-07 15:37:37 +0800

    test(server): add basic server test - Create ServerTest class with a simple contextLoads test - Add necessary imports for Spring Boot test context

---
+++ server/src/test/kotlin/me/ahoo/wow/template/server/ServerTest.kt
@@ -0,0 +1,12 @@
+package me.ahoo.wow.template.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerTest {
+
+
+    @Test
+    fun contextLoads() = Unit
+}
