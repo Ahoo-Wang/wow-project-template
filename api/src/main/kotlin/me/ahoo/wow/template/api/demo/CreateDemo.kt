ef104d213a41bbcdc244b9bd8610db87c2349711
Ahoo Wang <ahoowang@qq.com>
2025-11-15 07:30:20 +0800

    feat(api): replace summary attribute with Summary annotation
    - Replaced CommandRoute.summary with @Summary annotation
    - Added Summary import to CreateDemo and UpdateDemo classes
    - Removed deprecated summary attribute from CommandRoute
    - Updated documentation comments for command classes
    - Ensured backward compatibility with existing API contracts
    - Improved code readability and maintainability

--- api/src/main/kotlin/me/ahoo/wow/template/api/demo/CreateDemo.kt
+++ api/src/main/kotlin/me/ahoo/wow/template/api/demo/CreateDemo.kt
@@ -3,12 +3,13 @@ package me.ahoo.wow.template.api.demo
 import me.ahoo.wow.api.annotation.CommandRoute
 import me.ahoo.wow.api.annotation.CreateAggregate
 import me.ahoo.wow.api.annotation.Event
+import me.ahoo.wow.api.annotation.Summary
 
+@Summary("创建Demo")
 @CreateAggregate
 @CommandRoute(
     method = CommandRoute.Method.POST,
     action = "",
-    summary = "创建Demo"
 )
 data class CreateDemo(
     override val data: String
--- api/src/main/kotlin/me/ahoo/wow/template/api/demo/UpdateDemo.kt
+++ api/src/main/kotlin/me/ahoo/wow/template/api/demo/UpdateDemo.kt
@@ -1,11 +1,12 @@
 package me.ahoo.wow.template.api.demo
 
 import me.ahoo.wow.api.annotation.CommandRoute
+import me.ahoo.wow.api.annotation.Summary
 
+@Summary("更新Demo")
 @CommandRoute(
     appendIdPath = CommandRoute.AppendPath.ALWAYS,
     action = "",
-    summary = "更新Demo"
 )
 data class UpdateDemo(
     override val data: String
